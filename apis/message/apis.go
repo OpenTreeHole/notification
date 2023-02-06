3b0f63afd456d555f744382720bcf9c5f194df38
Ke Chen <2697947313@qq.com>
2023-02-06 14:22:26 +0800

    refactor: clean unnecessary models

--- apis/message/apis.go
+++ apis/message/apis.go
@@ -8,6 +8,10 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+func RegisterRoutes(app fiber.Router) {
+	app.Post("/messages", SendMessage)
+}
+
 // SendMessage
 // @Summary Send a message
 // @Description Send to multiple recipients and save to db, admin only.
@@ -17,22 +21,13 @@ import (
 // @Router /messages [post]
 // @Success 201 {object} Message
 func SendMessage(c *fiber.Ctx) error {
-	var body CreateModel
-	err := ValidateBody(c, &body)
+	var message Message
+	err := ValidateBody(c, &message)
 	if err != nil {
 		return err
 	}
 
-	message := Message{
-		Type:        body.Type,
-		Title:       body.Title,
-		Description: body.Description,
-		Data:        body.Data,
-		URL:         body.URL,
-		Recipients:  body.Recipients,
-	}
-
 	go push.Send(message)
 
-	return Serialize(c.Status(201), &message)
+	return c.Status(201).JSON(message)
 }
--- models/base.go
+++ models/base.go
@@ -2,59 +2,17 @@
 package models
 
 import (
-	"database/sql/driver"
 	"notification/config"
-	"time"
-
-	"github.com/goccy/go-json"
-	"gorm.io/gorm"
-	"gorm.io/gorm/schema"
 )
 
 var DB = config.DB
 
 type Map = map[string]any
 
-type BaseModel struct {
-	ID        int       `gorm:"primarykey" json:"id"`
-	CreatedAt time.Time `json:"time_created"`
-	UpdatedAt time.Time `json:"time_updated"`
-}
-
-func (model BaseModel) GetID() int {
-	return model.ID
+type MessageModel struct {
+	Message string `json:"message"`
 }
 
 type Models interface {
 	PushToken | []PushToken
-}
-
-type JSON map[string]any
-
-func (t JSON) Value() (driver.Value, error) {
-	return json.Marshal(t)
-}
-
-func (t *JSON) Scan(input any) error {
-	return json.Unmarshal(input.([]byte), t)
-}
-
-// GormDataType gorm common data type
-func (JSON) GormDataType() string {
-	return "json"
-}
-
-// GormDBDataType gorm db data type
-//
-//goland:noinspection GoUnusedParameter
-func (JSON) GormDBDataType(db *gorm.DB, field *schema.Field) string {
-	switch db.Dialector.Name() {
-	case "sqlite":
-		return "JSON"
-	case "mysql":
-		return "JSON"
-	case "postgres":
-		return "JSONB"
-	}
-	return ""
 }
